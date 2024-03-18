99cca59e4ca697e9160d01417fca3bbfb2699240
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-18 19:53:14 +0300

    updating project to add users app

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/profile/', user_views.home, name='home'),
+    path('register/', user_views.register, name='register'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
